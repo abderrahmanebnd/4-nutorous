2ccc7d082290f45cd3876e1d85ceb46b3fcb0e98
Abderrahmane bnd <abdoubendaia7@gmail.com>
2024-12-14 17:31:58 +0100

    refactor:api features

--- controllers/tourController-v1.js
+++ controllers/tourController-v1.js
@@ -1,5 +1,5 @@
-const Tour = require('./../models/tourModel.js');
-const APIFeatures = require('./../utils/apiFeatures.js');
+const Tour = require('../models/tourModel.js');
+
 // JSend specification is a send format {status,data}
 
 exports.aliasTopTours = (req, res, next) => {
@@ -8,18 +8,53 @@ exports.aliasTopTours = (req, res, next) => {
   req.query.fields = 'name,price,ratingAverage,summary,difficulty';
   next();
 };
-
 exports.getAllTours = async (req, res) => {
   try {
+    // BUILD THE QUERY
+    // 1A) Filtering
+    const queryObj = { ...req.query };
+    const excludeFields = ['page', 'limit', 'sort', 'fields'];
+    excludeFields.forEach((el) => delete queryObj[el]);
+
+    // 1B) Advanced Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = JSON.parse(
+      queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`),
+    );
+
+    // {duration:{$gte:5}}
+    // {duration:{gte:"5"}}
+    // gte,gt,lte,lt
+    let query = Tour.find(queryStr);
+
+    // 2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+      // (sort ratingAverage ) here rating average is the second criteria to sort with in postman do sort=price,averageRating with the comma
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    // 3) Fields limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v'); // using the minus '-' this means select everything expect the __v field
+    }
+
+    // 4) Pagination
+    const page = req.query.page * 1 || 1; // to convert the page from string to number
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    // page=3&limit=10, 1-10 page 1, 11-20 page 2, 21-20 page 3
+    query = query.skip(20).limit(10);
+    query = query.skip(skip).limit(limit);
+
     // EXECUTE THE QUERY
-    console.log('heelo');
-    const features = new APIFeatures(Tour.find(), req.query)
-      .filter()
-      .sort()
-      .limitFields()
-      .paginate();
-    console.log(features);
-    const tours = await features.query;
+    const tours = await query;
 
     // SEND RESPONSE
     res.status(200).json({
