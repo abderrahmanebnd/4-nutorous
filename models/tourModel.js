7cc4e10f377f491fd761292a3fd6a9eccb91c1e7
Abderrahmane bnd <abdoubendaia7@gmail.com>
2024-12-15 21:09:05 +0100

    FEAT: DOCUMENT MIDDLEWARE

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,68 +1,82 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    // this called schema option
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-    trim: true,
-  },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration'],
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a group Size'],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have a difficulty'],
-  },
-  ratingsAverage: { type: Number, default: 4.5 },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-  priceDiscount: Number,
-  // trim removes all the whitespaces
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, 'A tour must have a description'],
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have a cover image'],
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(), // In mongoose, it  automatically be converted to today date.
-    select: false,
-  },
-  startDates: {
-    type: [Date],
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      // this called schema option
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+      trim: true,
+    },
+    slug: String, // slug be like : 'name-lolo-coco'
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group Size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+    },
+    ratingsAverage: { type: Number, default: 4.5 },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price'],
+    },
+    priceDiscount: Number,
+    // trim removes all the whitespaces
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a description'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image'],
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(), // In mongoose, it  automatically be converted to today date.
+      select: false,
+    },
+    startDates: {
+      type: [Date],
+    },
   },
   {
-    toJSON :{virtuals:true},
-    toObject :{virtuals:true}
-  }
-});
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
+);
 
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
   // this here is refered to the current document
 });
+
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+tourSchema.post('save', function (doc, next) {
+  console.log(doc); // the created or the saved document
+  next();
+});
+
 // is like a convention to always put the models name to uppercase
 const Tour = mongoose.model('Tour', tourSchema);
 
