ac000c417f2f08b639dbacb8304fb68f7a3bcae9
Abderrahmane bnd <abdoubendaia7@gmail.com>
2024-12-27 16:10:13 +0100

    feat:Adding a /me endpoint

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,6 +7,7 @@ const {
   deleteUser,
   updateMe,
   deleteMe,
+  getMe,
 } = require('./../controllers/userController');
 
 const {
@@ -29,6 +30,7 @@ router.patch('/resetPassword/:token', resetPassword);
 
 router.patch('/updateMyPassword', protect, updatePassword);
 
+router.get('/me', protect, getMe, getUser);
 router.patch('/updateMe', protect, updateMe);
 router.delete('/deleteMe', protect, deleteMe);
 
